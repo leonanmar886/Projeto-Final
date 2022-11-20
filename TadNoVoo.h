422b0aa0c4154a8f25515278cd8978fa9da79d21
gcirineu18 <80009698+gcirineu18@users.noreply.github.com>
2022-11-20 11:07:43 -0300

    Delete TadNoVoo.h

--- TadNoVoo.h
+++
@@ -1,8 +0,0 @@
-typedef struct no_voo No_Voo;
-typedef struct lista_voo Lista;
-
-// cria uma lista encadeada de voos
-Lista *cria_lista();
-
-// libera uma lista existente e retorna 1 se foi possível e -1 se não foi possível
-int libera_lista(Lista **lista);
