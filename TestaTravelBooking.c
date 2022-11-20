0cf76630fa7ee0a69746d5959e13d2649690cd1e
Lucas Braide <107264197+lucasbraide@users.noreply.github.com>
2022-11-20 10:50:30 -0300

    Implementação Testes de Voo

--- TestaTravelBooking.c
+++ TestaTravelBooking.c
@@ -23,7 +23,7 @@ int teste_passageiro_novo(){
   return 1;
 }
 
-float teste_aluno_acessa_com_dados_validos() {
+float teste_passageiro_acessa_com_dados_validos() {
   int id;
   char nome[50];
   char endereco[100];
@@ -152,10 +152,11 @@ int teste_fila_passageiros_insere() {
   return 0;
 }
 
-int main(void) {
-  printf("Hello World, Equipe\n");
-  return 0;
-}
+//*******************************************
+//*******************************************
+//---------------------------Testes relacionados a Voo-----------------------------------------------------------------------------
+//*******************************************
+//*******************************************
 
 int testa_voo_novo(){
   int codigo_valido = 550;
@@ -172,15 +173,15 @@ int testa_voo_novo(){
   Voo* Voo_Nulo = voo_novo(NULL, origem_invalido, origem_invalido);
 
   if (Voo_Valido == NULL || Voo_Igual != NULL || Voo_Invalido != NULL || Voo_Nulo != NULL){
-    printf("Voo_Cria: [FALHOU]");
+    printf("[Falhou] - voo_cria()\n");
     return 0;
   }
   else if(Voo_Valido != NULL && Voo_Igual == NULL || Voo_Invalido == NULL || Voo_Nulo == NULL){
-    printf("Voo_Cria: [PASSOU]");
+    printf("[Passou] - voo_cria()\n");
     return 1;
   }
   else{
-    printf("Voo_Cria: [ERRO]");
+    printf("[Erro] - voo_cria()\n");
     return -1;
   }
 }
@@ -199,16 +200,295 @@ int testa_voo_libera(){
   int aux_1 = voo_libera(Voo_Valido);
   int aux_2 = voo_libera(Voo_Nulo);
     if (aux_1 != 1 || aux_2 != 0 || Voo_Valido != NULL || Voo_Nulo != NULL){
-    printf("Voo_Libera: [FALHOU]");
+    printf("[Falhou] - voo_libera() \n");
     return 0;
   }
   else if (aux_1 == 1 && aux_2 == 0 && Voo_Valido == NULL && Voo_Nulo == NULL){
-    printf("Voo_Libera: [PASSOU]")
+    printf("[Passou] - voo_libera() \n")
     return 1;
   }
   else{
-    printf("Voo_Libera[FALHOU]");
+    printf("[Falhou] - voo_libera() \n");
     return -1
   }
+}
+
+int testa_voo_acessa(){
+  int codigo;
+  char origem[100];
+  char destino[100];
+  Voo* Voo_Aux = voo_novo(1590, "Santiago", "Rio de Janeiro");
+  if (Voo_Aux != NULL){
+    voo_acessa(Voo_Aux, &(codigo), origem, destino);
+    if (codigo == 1590 && strcmp(origem, "Santiago" == 0 && strcmp(destino, "Rio de Janeiro") == 0)){
+      printf("[Passou] - voo_acessa() \n");
+      return 1;
+    }
+    else{
+      printf("[Falhou] - voo_acessa() \n");
+    }
+  }
+}
+
+float teste_voo_atribui_dados_validos() {
+  int codigo;
+  char origem[100];
+  char destino[100];
+  
+  Voo *voo = voo_novo(1, "Fortaleza", "Coritiba");
+  if (voo != NULL) {
+    voo_atribui(voo, 2, "Fortaleza", "Recife");
+    voo_acessa(voo, &codigo, origem, destino);
+    if (codigo == 2 && strcmp(origem, "Fortaleza") == 0 &&
+        strcmp(destino, "Recife") == 0) {
+      printf(
+          "[Passou] - voo_atribui()\n");
+      return 1;
+    } else {
+      printf(
+          "[Falhou] - voo_atribui()\n");
+          return 0;
+        } 
+  } else {
+    printf(
+        "[Falhou] - voo_atribui()\n");
+        return 0;
+  }
+}
+
+float teste_voo_atribui_com_dados_invalidos() {
+  int codigo;
+  char origem[55] = "N";
+  char destino[35] = "C";
+
+  for (int i = 0; i < 53; i++) {
+    strcat(origem, "N");
+  }
+
+  for (int i = 0; i < 33; i++) {
+    strcat(destino, "C");
+  }
+
+  Voo *voo = voo_novo(1, "Belem", "Fortaleza");
+  if (voo != NULL) {
+    voo_atribui(voo, 2, origem, destino);
+    voo_acessa(voo, &matricula, nome, curso);
+    if (codigo == 1 || strcmp(nome, "Belem") == 0 ||
+        strcmp(curso, "Fortaleza") == 0) {
+          voo_atribui(NULL, NULL, NULL, NULL);
+          voo_acessa(voo, &matricula, nome, curso);
+    if (codigo == 1 || strcmp(nome, "Belem") == 0 ||
+        strcmp(curso, "Fortaleza") == 0) {
+      printf("[Passou] - voo_atribui() \n");
+      return 1;
+    } else {
+      printf("[Falhou] - voo_atribui() \n");
+    }
+  } else {
+    printf(
+        "[Falhou] - voo_atribui()\n");
+  }
+  return 0;
+}
+}
+
+int testa_voo_igual(){
+  Voo* Voo_1 = voo_novo(4, "Paris", "Vitoria");
+  Voo* Voo_2 = voo_novo(4, "Paris", "Vitoria");
+  if (voo_igual(Voo_1, Voo_2) == 1){
+    printf("[Passou] - voo_igual()\n");
+    return 1;
+  }
+  printf("[Falhou] - voo_igual()");
+  return 0;
+}
+
+int teste_fila_voo_cria() {
+  lista_voo *fila = fila_voo_cria();
+  if (fila != NULL) {
+    printf("[Passou] - fila_voo_cria()\n");
+    return 1;
+  } else {
+    printf("[Falhou] - fila_voo_cria()\n");
+    return 0;
+  }
+}
+
+float teste_fila_voo_libera() {
+  float pontuacao = 0;
+  lista_voo *fila = fila_voo_cria();
+  fila_voo_libera(&fila);
+  if (fila != NULL || fila_voo_libera(NULL) != 0) {
+    printf(
+        "[Falhou] - fila_voo_libera()\n");
+    return 0;
+  } else {
+    printf(
+        "[Passou] - fila_voo_libera()\n");
+    return 1;
+  }
+}
+
+float teste_fila_voo_vazia() {
+  lista_voo *fila = fila_voo_cria();
+  if (fila != NULL && fila_voo_vazia(fila) == 1) {
+    printf("[Passou] - fila_voo_vazia() \n");
+    return 1;
+} 
+  Voo* voo_aux = voo_novo(1,"Maceio", "Campinas");
+  fila_voo_insere(fila, voo_aux);
+
+  if (fila_voo_vazia(fila) != 0){
+    sprintf("[Falhou] - fila_voo_vazia() \n");
+    return 0;
+  }
+
+  
+  else {
+    printf("[Falhou] - fila_voo_vazia() \n");
+    return 0;
+  }
+}
+
+float teste_fila_voo_insere() {
+  lista_voo *fila = fila_voo_cria();
+  if (fila != NULL) {
+    Voo *voo_manaus = voo_novo(1, "Manaus", "Fortaleza");
+    Voo *voo_coritiba = voo_novo(1, "Coritiba", "Fortaleza");
+    if (fila_voo_insere(fila, voo_manaus) == 1 && fila_voo_insere(fila, voo_coritiba) != 0 && fila_insere(fila, NULL) == -1) {
+      Voo *voo_palmas = voo_novo(2, "Palmas", "Fortaleza");
+      Voo *voo_recife = voo_novo(2, "Recife", "Fortaleza");
+      if (fila_voo_insere(fila, voo_palmas) == 1 && fila_voo_insere(fila, voo_recife) != 0) {
+        Voo *voo_orlando = voo_novo(3, "Orlando", "Fortaleza");
+        if (fila_voo_insere(fila, jose) == 1) {
+          printf("[Passou] - fila_voo_insere\n");
+          return 1;
+        } else {
+          printf("[Falhou] - fila_voo_insere()\n");
+        }
+      } else {
+        printf("[Falhou] - fila_voo_insere()\n");
+      }
+    } else {
+      printf("[Falhou] - fila_voo_insere()\n");
+    }
+  } else {
+    printf(
+        "[Falhou] - fila_voo_insere()\n");
+  }
+  return 0;
+}
+
+float teste_fila_voo_primeiro() {
+  lista_voo *fila = fila_voo_cria();
+  if (fila != NULL) {
+    Voo *voo_fortaleza = voo_novo(1, "Recife", "Fortaleza");
+    fila_voo_insere(fila, voo_fortaleza);
+    Voo* voo_paris = voo_novo(2, "Palmas", "Paris");
+    fila_voo_insere(fila, voo_paris);
+    Voo *voo_manaus = voo_novo(3, "Miami", "Manaus");
+    fila_voo_insere(fila, voo_manaus);
+    Voo *voo_primeiro = fila_primeiro(fila);
+
+    if (voo_igual(voo_primeiro, voo_fortaleza) == 1) {
+      printf("[Passou] - fila_voo_primeiro()\n");
+      return 1;
+
+    } else {
+      printf("[Falhou] - fila_voo_primeiro()\n");
+    }
+  } else {
+    printf(
+        "[Falhou] - fila_voo_primeiro()\n");
+  }
+  return pontuacao;
+}
+
+float teste_fila_voo_busca() {
+  int codigo;
+  char origem[100];
+  char destino[100];
+
+  lista_voo *fila = fila_voo_cria();
+  if (fila != NULL) {
+    Voo *voo_fortaleza = voo_novo(1, "Recife", "Fortaleza");
+    fila_voo_insere(fila, voo_fortaleza);
+    Voo* voo_paris = voo_novo(2, "Palmas", "Paris");
+    fila_voo_insere(fila, voo_paris);
+    Voo *voo_manaus = voo_novo(3, "Maceio", "Porto Alegre");
+    fila_voo_insere(fila, voo_manaus);
+    Voo *aux = fila_busca(fila, 2);
+    if (aux != NULL) {
+      voo_acessa(aux, &codigo, origem, destino);
+      if (codigo == 2 && strcmp(origem, "Palmas") == 0 &&
+          strcmp(curso, "Paris") == 0) {
+        printf("[Passou] - fila_voo_busca()\n");
+               return 1;
+      } else {
+        printf("[Falhou] - fila_voo_busca()\n");
+      }
+    } else {
+      printf("[Falhou] - fila_voo_busca()\n");
+    }
+  } else {
+    printf("[Falhou] - fila_voo_busca()\n");
+  }
+  return 0;
+}
+
+float teste_fila_voo_retira() {
+  lista_voo *fila = fila_voo_cria();
+  if (fila != NULL) {
+    Voo *voo_fortaleza = voo_novo(1, "Recife", "Fortaleza");
+    fila_voo_insere(fila, voo_fortaleza);
+    Voo* voo_paris = voo_novo(2, "Palmas", "Paris");
+    fila_voo_insere(fila, voo_paris);
+    Voo *voo_manaus = voo_novo(3, "Maceio", "Porto Alegre");
+    fila_voo_insere(fila, voo_manaus);
+    Voo *aux = fila_voo_retira(fila);
+
+    if (voo_igual(aux, voo_fortaleza) == 1) {
+      aux = fila_voo_retira(fila);
+      if (voo_igual(aux, voo_paris) == 1) {
+        aux = fila_voo_retira(fila);
+        if (voo_igual(aux, voo_manaus) == 1) {
+          aux = fila_voo_retira(fila);
+          if(aux == NULL){
+          printf("[Passou] - fila_voo_retira() \n");
+          return 1;}
+          else{
+            printf("[Falhou] - fila_voo_retira()\n");
+            return 0;
+          }
+        }
+         else {
+          printf("[Falhou] - fila_voo_retira()\n");
+        }
+      } else {
+        printf("[Falhou] - fila_voo_retira()\n");
+      }
+    } else {
+      printf(
+          "[Falhou] - fila_voo_retira()\n");
+    }
+  } else {
+    printf(
+        "[Falhou] - fila_voo_retira()\n");
+  }
+  return 0;
+}
+
+int main(void) {
+  printf("Hello World, Equipe\n");
+  return 0;
+}
+
+
+int main(void) {
+  printf("Hello World, Equipe\n");
+  return 0;
+}
+
+
 
 
