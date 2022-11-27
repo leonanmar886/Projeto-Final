c9522758a66dc7c942413b7aa8eeceb656bbae2e
leonan <leonan.marques886@gmail.com>
2022-11-27 12:53:46 -0300

    Feature: função trecho_acessa implementada

--- Trecho.c
+++ Trecho.c
@@ -8,6 +8,14 @@ struct trecho {
   struct trecho *proximo;
 };
 
+struct reserva {
+  int codigo;
+  Data *data_viagem;
+  Passageiro *passageiro;
+  Voo *voo;
+  Assento assento;
+};
+
 Trecho* novo_trecho(Reserva* reserva, Trecho* proximo_trecho){
     if(reserva == NULL || proximo_trecho == NULL){
         return NULL;
@@ -20,3 +28,22 @@ Trecho* novo_trecho(Reserva* reserva, Trecho* proximo_trecho){
     return trecho;
 };
 
+void trecho_libera(Trecho** trecho){
+    if(trecho == NULL){
+        return NULL;
+    }
+
+    free(*trecho);
+    trecho = NULL;
+};
+
+void trecho_acessa(Trecho* trecho, Reserva* reserva, Trecho* proximo_trecho){
+    if(trecho == NULL){
+        return NULL;
+    }
+    Reserva* reserva_aux = trecho->reserva;
+    Trecho* trecho_aux = trecho->proximo;
+
+    *(reserva) = *(reserva_aux);
+    *(proximo_trecho) = *(trecho_aux);
+};
